207b00856d4aca3e0885cf4df351b2368fe763c2
Abdul Samad <asamad@adobe.com>
2024-07-31 18:59:03 +0530

    removing loader from select

--- src/test/java/run/VerifyApplication.java
+++ src/test/java/run/VerifyApplication.java
@@ -46,6 +46,9 @@ public class VerifyApplication {
                         search.sendKeys(Keys.ENTER);
                         Thread.sleep(2000);
                         Utility.clickByJavaScript(Utility.getLocator("//td[contains(text(),\"" + enrolmentNumber + "\")]/..//button[@title=\"Verify\"]", "xpath"));
+                        Thread.sleep(1000);
+                        WebElement loader = Utility.getLocator("global-loader", "id");
+                        ((JavascriptExecutor) Utility.driver).executeScript("arguments[0].remove();", loader);
                         select = new Select(Utility.getLocator("id_application_status", "id"));
                         select.selectByVisibleText("Verify");
                         Utility.clickByJavaScript(Utility.getLocator("save_form_verify", "id"));
